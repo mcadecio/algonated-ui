5bd58375ea40659cf2c23003c3d638f90a6e5443
Dercio Daio <ddaio@mimecast.com>
2021-01-22 09:34:21 +0000

    Updating config var

--- src/components/exercise/ExercisePage.js
+++ src/components/exercise/ExercisePage.js
@@ -47,7 +47,7 @@ const ExerciseCodingArea = ({exercise, animation}) => {
             code: value
         };
         console.log({request});
-        console.log(process.env);
+        console.log(process.env.FYP_SERVER_DOMAIN);
         let endpoint = `${process.env.FYP_SERVER_DOMAIN}${exercise.endpoint}`;
 
         if (process.env.NODE_ENV === 'development') {
